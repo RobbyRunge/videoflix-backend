50d7200ff55c367312e83528772d3760d099e330
RobbyR98 <robbyrunge@yahoo.com>
2025-12-15 20:33:02 +0100

    feat: add signal handler for video deletion to remove video files

--- video_content_app/api/signals.py
+++ video_content_app/api/signals.py
@@ -1,6 +1,8 @@
-from video_content_app.models import Video
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+import os
+
+from video_content_app.models import Video
 
 
 @receiver(post_save, sender=Video)
@@ -11,3 +13,14 @@ def video_created_handler(sender, instance, created, **kwargs):
     """
     if created:
         pass  # Placeholder for actions to perform on video creation
+
+
+@receiver(post_delete, sender=Video)
+def video_deleted_handler(sender, instance, **kwargs):
+    """
+    Signal handler for post_delete signal of Video model.
+    Perform actions when a video is deleted.
+    """
+    if instance.video_file:  # For later, update this one for different videos (480p, 720p, etc.)
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
