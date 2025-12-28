d3112f41f201a0230bd5d8edc0ecc9fa01947f3f
RobbyR98 <robbyrunge@yahoo.com>
2025-12-28 19:48:10 +0100

    feat: add entrypoint scripts for development and production environments with superuser creation

---
+++ backend.entrypoint.dev.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e 
+
+echo "Warte auf PostgreSQL auf $DB_HOST:$DB_PORT..."
+
+while ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -q; do
+    echo "PostgreSQL ist nicht erreichbar - schlafe 1 Sekunde"
+    sleep 1
+done
+
+echo "PostgreSQL ist bereit - fahre fort..."
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
+
+if not User.objects.filter(username=username).exists():
+    print(f"Creating superuser '{username}'...")
+    
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' created.")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
+# Worker wird gestartet
+python manage.py rqworker default &
+# --reload für schnelle Codeänderungen während der Entwicklung
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --reload
